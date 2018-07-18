1ac64031ed016ad13c667dbbcd9956e5abf70df2
汪晓霞 <xiaoxia.wang@dingdandao.com>
2018-07-18 22:59:23 +0800

    完善demo

--- src/components/transfer/index.vue
+++ src/components/transfer/index.vue
@@ -1,45 +1,37 @@
 <template>
   <transition name="fade">
-    <!-- <div class="mask"></div> -->
-    <div class="tl-transfer">
-      <div class="transfer-main" v-show="visible">
-        <div class="transfer-wrap_left">
-          <!-- <div class="dt-search">
-            <input v-model="keyWords" type="text" class="dd-input input-search" :placeholder="`搜索${type}名称/${type}首字母`">
-            <img class="search-icon" src="//static.dingdandao.com/vipSearch.png">
-          </div> -->
-          <div class="transfer-goods-list">
-            <div class="list-label">选择{{type}}</div>
-            <div v-for="goods in gdList" :key="goods.value" class="transfer-item" @click="handleClick" v-show='!goods.unShow'>
-              <!-- <div> -->
-                <!-- <label v-if="goods.subTypeName !== undefined" class="transfer-label category">{{ goods.subTypeName }}</label> -->
-              <label class="transfer-item_label">{{ goods[options.label] }}</label>
-              <!-- </div> -->
-              <input v-model="goods[options.selected]" type="checkbox" class="checkbox transfer-item_checkBox">
+    <div class="tl-wrap" v-show="visible">
+      <div class="mask" @click="handleCancel"></div>
+      <div class="tl-transfer">
+        <div class="transfer-main">
+          <div class="transfer-wrap_left">
+            <div class="transfer-goods-list">
+              <div class="list-label">选择{{type}}</div>
+              <div v-for="goods in gdList" :key="goods.value" class="transfer-item" @click="handleClick" v-show='!goods.unShow'>
+                <label class="transfer-item_label">{{ goods[options.label] }}</label>
+                <input v-model="goods[options.selected]" type="checkbox" class="checkbox transfer-item_checkBox">
+              </div>
             </div>
           </div>
-        </div>
-        <div class="transfer-wrap_right">
-          <div class="transfer-selected-list">
-            <div class="list-label">已选择{{ categoryName }} {{ rightData.length || 0 }} {{typeUnit}}{{type}}</div>
-            <div v-for="(item, index) in rightData" :key="item[options.id]" class="transfer-item transfer-item_selected">
-              <!-- <div> -->
-                <!-- <label v-if="item.subTypeName !== undefined" class="transfer_label_selected category">{{ item.subTypeName }}</label> -->
-              <label class="transfer-label-selected">{{ item[options.label] }}</label>
-              <!-- </div> -->
-              <div class="transfer-remove-btn" @click="handleRemove(item[options.id], index)">
-                <span aria-hidden="true">&times;</span>
+          <div class="transfer-wrap_right">
+            <div class="transfer-selected-list">
+              <div class="list-label">已选择 {{ rightData.length || 0 }} {{typeUnit}}{{type}}</div>
+              <div v-for="(item, index) in rightData" :key="item[options.id]" class="transfer-item transfer-item_selected">
+                <label class="transfer-label-selected">{{ item[options.label] }}</label>
+                <div class="transfer-remove-btn" @click="handleRemove(item[options.id], index)">
+                  <span aria-hidden="true">&times;</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <slot name="footer">
+          <div class="transfer-footer">
+            <button class="btn-transfer btn-confirm" @click="handleConfirm">确定</button>
+            <button class="btn-transfer btn-cancel" @click="handleCancel">取消</button>
+          </div>
+        </slot>
       </div>
-      <slot name="footer">
-        <div class="transfer-footer">
-          <button class="btn-transfer btn-cancel" @click="handleCancel">取消</button>
-          <button class="btn-transfer btn-confirm" @click="handleConfirm">确定</button>
-        </div>
-      </slot>
     </div>
   </transition>
 </template>
@@ -56,21 +48,22 @@ export default {
         }
       }
     },
+    // 选择的物品名称
     type: {
       type: String,
-      default: '商品' // 根据type显示类型文字 1：商品 2：房间
+      default: ''
+    },
+    // 选择的物品单位
+    typeUnit: {
+      type: String,
+      default: ''
     },
     // 是否显示
     visible: {
       type: Boolean,
       default: false
     },
-    // 商品分类名称
-    categoryName: {
-      type: String,
-      default: ''
-    },
-    // 商品列表
+    // 物品列表
     goodsNodeList: {
       type: Array,
       default () {
@@ -94,25 +87,13 @@ export default {
   },
   data () {
     return {
-      keyWords: '', // 搜索关键字
-      gdList: this.goodsNodeList, // 商品列表,
-      tempList: this.goodsNodeList
+      gdList: this.goodsNodeList // 商品列表,
     }
   },
-  // 遮罩
-  // mixins: [popup],
   watch: {
     goodsNodeList () {
       // 深拷贝
       this.gdList = JSON.parse(JSON.stringify(this.goodsNodeList))
-      this.tempList = JSON.parse(JSON.stringify(this.goodsNodeList))
-      this.initList()
-    },
-    keyWords () {
-      this.search()
-    },
-    visible () {
-      this.keyWords = ''
     }
   },
   computed: {
@@ -125,66 +106,9 @@ export default {
         }
       })
       return result
-    },
-    typeUnit () {
-      let str
-      switch (this.type) {
-        case '商品':
-          str = '件'
-          break
-        case '房间':
-          str = '间'
-      }
-      return str
     }
   },
   methods: {
-    /**
-     * 初始化
-     */
-    initList () {
-      this.gdList.map(goods => {
-        // goods.spell = PySearch(goods[this.options.label])
-        // if (goods.subTypeName !== undefined) {
-        // goods.subSpell = PySearch(goods.subTypeName)
-        // }
-      })
-    },
-    /**
-     * 搜索
-     * 案商品拼音首字母进行搜索
-     */
-    search () {
-      console.log(this.gdList)
-      // this.gdList.forEach(goods => {
-      //   // const keyCode = PySearch(this.keyWords)[0].toUpperCase()
-      //   const nameFlag = goods[this.options.label].indexOf(this.keyWords) >= 0
-      //   const subTypeName =
-      //     goods.subTypeName !== undefined
-      //       ? goods.subTypeName.indexOf(this.keyWords) >= 0
-      //       : false
-      //   let pyFlag = false
-      //   if (goods.spell && goods.spell.length) {
-      //     goods.spell.forEach(el => {
-      //       if (el.indexOf(keyCode) >= 0) {
-      //         pyFlag = true
-      //       }
-      //     })
-      //   }
-      //   if (goods.subSpell && goods.subSpell.length) {
-      //     goods.subSpell.forEach(el => {
-      //       if (el.indexOf(keyCode) >= 0) {
-      //         pyFlag = true
-      //       }
-      //     })
-      //   }
-      //   if (pyFlag || nameFlag || subTypeName) {
-      //     this.$set(goods, 'unShow', false)
-      //   } else {
-      //     this.$set(goods, 'unShow', true)
-      //   }
-      // })
-    },
     /**
      * 确定提交事件
      */
@@ -239,70 +163,86 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.tl-transfer {
-  width: 600px;
-  height: 540px;
-  background-color: #ffffff;
-  border-radius: 8px;
-  box-shadow:0 2px 4px 0 rgba(0,0,0,0.15);
-  color: #666666;
-  .transfer-main {
-    display: flex;
-    height: 430px;
-    border-bottom: 1px solid #fbfbfb;
-    .transfer-item {
-      padding-top: 10px;
-      display: flex;
-      height: 40px;
-      align-items: center;
-      .transfer-item_label {
-        margin-right: 8px;
-      }
-    }
-    .list-label {
-      font-size: 12px;
-    }
-    .transfer-wrap_left {
-      width: 50%;
-      padding: 16px;
-      .transfer-item_checkBox {
-        cursor: pointer;
-      }
-    }
-    .transfer-wrap_right {
-      width: 50%;
-      border-left: 1px solid #fbfbfb;;
-      padding: 8px 18px 0;
-      .transfer-item_selected {
-        justify-content: space-between;
-      }
-      .transfer-remove-btn {
-        cursor: pointer;
-      }
-    }
+.tl-wrap {
+  .mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 99;
+    background-color: rgba(0, 0, 0, 0.4)
   }
-  .transfer-footer {
-    height: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    .btn-transfer {
-      width: 120px;
-      height: 36px;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-      &:hover, &:focus, &:active {
-        outline: none;
+  .tl-transfer {
+    position: absolute;
+    z-index: 100;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 600px;
+    height: 540px;
+    background-color: #ffffff;
+    border-radius: 8px;
+    box-shadow:0 2px 4px 0 rgba(0,0,0,0.15);
+    color: #666666;
+    .transfer-main {
+      display: flex;
+      height: 448px;
+      border-bottom: 1px solid #ebebeb;
+      .transfer-item {
+        padding-top: 10px;
+        display: flex;
+        height: 40px;
+        align-items: center;
+        .transfer-item_label {
+          margin-right: 8px;
+        }
+      }
+      .list-label {
+        font-size: 12px;
+      }
+      .transfer-wrap_left {
+        width: 50%;
+        padding: 16px;
+        .transfer-item_checkBox {
+          cursor: pointer;
+        }
+      }
+      .transfer-wrap_right {
+        width: 50%;
+        border-left: 1px solid #ebebeb;;
+        padding: 16px 18px 0;
+        .transfer-item_selected {
+          justify-content: space-between;
+        }
+        .transfer-remove-btn {
+          cursor: pointer;
+        }
       }
     }
-    .btn-cancel {
-      border: 1px solid #4285F6;
-      color: #666666;
-    }
-    .btn-confirm {
-      background-color: #4285F6;
-      color: #ffffff;
+    .transfer-footer {
+      height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      .btn-transfer {
+        width: 120px;
+        height: 36px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        &:hover, &:focus, &:active {
+          outline: none;
+        }
+      }
+      .btn-cancel {
+        border: 1px solid #4285F6;
+        color: #666666;
+      }
+      .btn-confirm {
+        background-color: #4285F6;
+        color: #ffffff;
+      }
     }
   }
 }
--- src/views/demos/transferDemo.vue
+++ src/views/demos/transferDemo.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="wrap-transfer_demo">
-    <transfer-demo :visible="true" :goodsNodeList="goodsList" :options="{id: 'goodsId', label: 'goodsName', selected: 'isSelected'}"></transfer-demo>
+    <button class="btn-select" @click="isShowTransfer = true">选择商品</button>
+    <div class="show-selected-items" v-if="selectedList && selectedList.length > 0">
+      <p>选中的商品有：</p>
+      <div v-for="item in selectedList" :key="item.id">{{ item.goodsName }}</div>
+    </div>
+    <transfer-demo
+      :visible.sync="isShowTransfer"
+      :goodsNodeList="goodsList"
+      :options="{id: 'goodsId', label: 'goodsName', selected: 'isSelected'}"
+      type="商品"
+      typeUnit="件"
+      :onConfirm="handleConfirm"
+      :onCancel="handleCancel">
+    </transfer-demo>
   </div>
 </template>
 <script>
@@ -8,17 +21,62 @@ import transferDemo from '@/components/transfer/index.vue'
 export default {
   data () {
     return {
-      goodsList: [
-        {
-          goodsId: 0,
-          goodsName: '商品一',
-          selected: false
-        }
-      ]
+      isShowTransfer: false,
+      selectedList: [],
+      goodsList: []
     }
   },
   components: {
     transferDemo
+  },
+  created () {
+    this.getGoodsList()
+  },
+  methods: {
+    getGoodsList () {
+      const mockData = {
+        'code': 1,
+        'data': {
+          'list': [
+            {
+              goodsId: 0,
+              goodsName: '商品一',
+              selected: false
+            },
+            {
+              goodsId: 1,
+              goodsName: '商品二',
+              selected: false
+            }
+          ]
+        },
+        'msg': '成功'
+      }
+      this.goodsList = mockData.data.list
+    },
+    handleConfirm (selectedItems) {
+      this.selectedList = selectedItems
+      this.isShowTransfer = false
+    },
+    handleCancel () {
+      this.isShowTransfer = false
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.wrap-transfer_demo {
+  .btn-select {
+    width: 120px;
+    height: 36px;
+    border: none;
+    background-color: #4285F6;
+    color: #ffffff;
+    border-radius: 4px;
+    cursor: pointer;
+    &:hover, &:focus, &:active {
+      outline: none;
+    }
+  }
+}
+</style>
